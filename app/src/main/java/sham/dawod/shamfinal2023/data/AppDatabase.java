f31ef927673d5474a9197cffcfb6115d5eceb667
Owner <alsalamcse>
2023-11-20 11:37:52 +0200

    HH

--- app/src/main/java/sham/dawod/shamfinal2023/data/AppDatabase.java
+++ app/src/main/java/sham/dawod/shamfinal2023/data/AppDatabase.java
@@ -18,68 +18,38 @@ version
  * الفئة المسؤولة عن بناء قاعدة البايانات بكل جداولها
  * وتوفر لنا كائن للتعامل مع قاعدة البيانات
  */
-public abstract class  AppDataBase extends RoomDatabase {
+public abstract class  AppDatabase extends RoomDatabase
+{
 
     /**
      * كائن للتعامل مع قاعدة البيانات
      */
-    private static AppDataBase db;
+    private static AppDatabase db;
 
     /**
      * يعيد كائن لعمليات جدول المستعملين
+     *
      * @return
      */
-    import android.content.Context;
-import androidx.room.Database;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
+    public abstract MyUserQuery getMyUserQuery();
 
-    /*
-    تعريف الجداول ورقم الاصدار
-    version
-    عند تغيير اي شي يخص جدول او جداول علينا تغيير رقم الاصدار
-    ليتم بناء قاعدة البيانات من جديد
-    */
-    @Database(entities = {MyUser.class, MySubject.class, MyTask.class}, version = 1)
-/**
- * الفئة المسؤولة عن بناء قاعدة البايانات بكل جداولها
- * وتوفر لنا كائن للتعامل مع قاعدة البيانات
- */
-    public abstract class AppDataBase extends RoomDatabase {
-
-        /**
-         * كائن للتعامل مع قاعدة البيانات
-         */
-        private static AppDataBase db;
-
-        /**
-         * يعيد كائن لعمليات جدول المستعملين
-         *
-         * @return
-         */
-        public abstract MyUserQuery getMyUserQuery();
-
-
-        /**
-         * بناء قاعدة البيانات واعادة كائن يؤشر عليها
-         *
-         * @param context
-         * @return
-         */
-
-
-        public static AppDataBase getDB(Context context) {
-            if (db == null) {
-                db = Room.databaseBuilder(context,
-                                AppDataBase.class,
-                                "samihDataBase")//اسم قاعدة اليانات
-                        .fallbackToDestructiveMigration()
-                        .allowMainThreadQueries()
-                        .build();
-            }
-            return db;
+    /**
+     * يعيد كائن لعمليات جدول الموضيع
+     *
+     * @return
+     */
+    public static AppDatabase getDB(Context context)
+    {
+        if (db == null)
+        {
+            db = Room.databaseBuilder(context,
+                            AppDatabase.class,
+                            "shamDataBase")//اسم قاعدة اليانات
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build();
         }
-
+        return db;
     }
 
 }
