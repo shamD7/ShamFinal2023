3767c1b6f448cbc20b858a11f6136a0ceb012723
Owner <alsalamcse>
2023-12-14 14:12:14 +0200

    HH

--- app/src/main/java/sham/dawod/shamfinal2023/ResMenu.java
+++ app/src/main/java/sham/dawod/shamfinal2023/ResMenu.java
@@ -8,6 +8,6 @@ public class ResMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.c_resmenu);
+        setContentView(R.layout.resmenu);
     }
 }
--- app/src/main/java/sham/dawod/shamfinal2023/favoriteRes.java
+++ app/src/main/java/sham/dawod/shamfinal2023/favoriteRes.java
@@ -3,11 +3,11 @@ package sham.dawod.shamfinal2023;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class OrestMenu extends AppCompatActivity {
+public class favoriteRes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.o_restmenu);
+        setContentView(R.layout.activity_favorite_res);
     }
 }
